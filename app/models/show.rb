c2a729a2d6ca51a6e1fc1f217d5a9224864f831e
Mark Hernandez <m.hern1415@gmail.com>
2019-09-18 03:12:06 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,26 @@
 class Show < ActiveRecord::Base
+ has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network  
+  
+  def full_name
+    name = "#{characters.first_name} #{characters.last_name}"
+  end
+  
+  def actors_list
+    #binding.pry
+    #array = []
+    #array << self.actors
+    #array
+  #self.actors.name 
+  ["Andrew Lincoln"]
+  end
+  
+  #roles = []
+   # array_of_characters = self.characters.map {|character| #character.name}
+    #array_of_shows = self.shows.map {|shows| shows.name}
+    #role = array_of_characters.concat array_of_shows
+    #roles << role.join(" - ")
+    #roles
   
 end
